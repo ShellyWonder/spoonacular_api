8b80aedbb34f7649738c6344770fa3f61f67b800
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2024-01-11 16:30:42 -0600

    Create MealPlanRestController.java

---
+++ src/main/java/com/wonderwebdev/spoonacular_api/web/MealPlanRestController.java
@@ -0,0 +1,54 @@
+package com.wonderwebdev.spoonacular_api.web;
+
+import java.net.MalformedURLException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+
+import com.wonderwebdev.spoonacular_api.dto.*;
+
+@RestController
+public class MealPlanRestController {
+	
+	@Value("${spoonacular.urls.base}")
+	private String baseUrl;
+	
+	@Value("${spoonacular.urls.mealplan}")
+	private String path;
+	
+	@Value("${spoonacular.api.key}")
+	private String apiKey;
+
+	@GetMapping("/mealplanner/week")
+	public ResponseEntity<WeekResponse>getWeekMeals(
+			@RequestParam(value="targetCalories", required = false)String numCalories,
+			@RequestParam(required = false)String diet,
+			@RequestParam(value="exclude", required = false)String exclusions) throws MalformedURLException{
+		
+		RestTemplate rt =new RestTemplate();
+		
+		URI uri = UriComponentsBuilder.fromHttpUrl(baseUrl + path)
+				.queryParam("apiKey", apiKey)
+				.queryParam("timeFrame", "week")
+				.queryParam("targetCalories", numCalories)
+				.queryParam("diet", diet)
+				.queryParam("exclude", exclusions)
+				.build()
+				.toUri();
+		
+		ResponseEntity<WeekResponse> response = rt.getForEntity(uri, WeekResponse.class);
+				
+		return response;
+		
+	}
+	
+	
+	
+}
